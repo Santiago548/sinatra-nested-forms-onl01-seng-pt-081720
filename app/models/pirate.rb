d931da7be9569efd1ede08907544dc58ea568b18
Dominic Barajas <dominicbarajas548@gmail.com>
2020-11-15 18:04:34 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(param)
+    @name = param[:name]
+    @weight = param[:weight]
+    @height = param[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
 end
